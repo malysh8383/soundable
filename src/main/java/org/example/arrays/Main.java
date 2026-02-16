136055e52ba5fdafb32ecb02a1b788dcf3eda655
malysh8383 <nik.malysh06@gmail.com>
2026-02-16 21:22:09 +0300

    first commit

--- src/main/java/org/example/arrays/Main.java
+++ src/main/java/org/example/arrays/Main.java
@@ -10,29 +10,45 @@ public class Main {
         ArrayList<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int choice;
+        boolean done = true;
 
-        while (true) {
+        while (done) {
+            System.out.println("1 - добавить задачу");
+            System.out.println("2 - удалить задачу по индексу");
+            System.out.println("3 - показать все задачи");
+            System.out.println("4 - показать количество задач");
+            System.out.println("5 - выход");
             System.out.println("Введите цифру от 1 до 5");
+
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Добавить задачу");
+                    String task = scanner.next();
+                    tasks.add(task);
+                    System.out.println("Задача добавлена");
                     break;
                 case 2:
-                    System.out.println("Удалить задачу по индексу");
+                    int index = scanner.nextInt();
+                    if (index >= 0 && index <= tasks.size()) {
+                        tasks.remove(index);
+                        System.out.println("Задача удалена");
+                    } else {
+                        System.out.println("Такого индекса нет");
+                    }
                     break;
                 case 3:
-                    System.out.println("Показать все задачи");
+                    System.out.println(tasks);
                     break;
                 case 4:
-                    System.out.println("Показать количество задач");
+                    System.out.println("Количество задач " + tasks.size());
                     break;
                 case 5:
+                    done = false;
                     System.out.println("выход");
                     break;
                 default:
                     System.out.println("Нет такой задачи");
-                    break;
+
             }
 
 
